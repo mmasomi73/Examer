a8c1025a6159eb61ec7ee7a2394800afd55a6bbd
mmasomi <mmasomi73@gmail.com>
2021-04-08 18:01:40 +0430

    livewire
    0.0.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('assets/css/tailwind.min.css')}}" rel="stylesheet">
     <title>Document</title>
     @livewireStyles
 </head>
 <body>
 
-    <livewire:auth.login />
+    @livewire('auth.login')
     @livewireScripts
 </body>
 </html>
